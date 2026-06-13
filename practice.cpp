2cd119de8d4c368a03cc439d02697c9e84f6db95
Deepak Maitry <110789176+DeepakMaitry@users.noreply.github.com>
2026-06-14 00:48:48 +0530

    14 june

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello, Deepak!" << endl << 
+            "Welcome to path to become millionare!" << endl << 
+            "You just need to show up regularly here" << endl << 
+            "regular presence is manditory even if it is for 1 hour." << endl;
+
+
+    return 0;
+}
